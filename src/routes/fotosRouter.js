b9db1bcf77970edb7f83a2bbde85a56cb864b57d
Yael <yaelsilvestre64@gmail.com>
2025-04-10 16:53:11 -0600

    Fotos router listas

--- src/routes/fotosRouter.js
+++ src/routes/fotosRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fotoController = require('../controllers/fotosControllers');
 
-router.post('/foto',fotoController.getFotos);
-router.get('/foto', fotoController.postFoto);
+router.post('/foto',fotoController.postFoto);
+router.get('/foto', fotoController.getFotos);
 
 module.exports = router;
