3b764bea1198c9ec28aa4e96ddf75338accbca57
Yael <yaelsilvestre64@gmail.com>
2025-04-12 00:19:42 -0600

    Estadisticas bien

--- src/routes/estadisticasRouter.js
+++ src/routes/estadisticasRouter.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const estadisticasController = require('../controllers/estadisticasControllers');
 
-// Ruta para obtener las ventas por zona
-router.get('/ventas-zona', estadisticasController.getVentasPorZona);
 
 // Ruta para obtener las ventas por mes
 router.get('/ventas-mes', estadisticasController.getVentasPorMes);
@@ -20,7 +18,5 @@ router.get('/producto-mas-vendido', estadisticasController.getProductoMasVendido
 // Ruta para obtener las ventas por día
 router.get('/ventas-dia', estadisticasController.getVentasPorDia);
 
-// Ruta para obtener el producto más vendido por zona
-router.get('/producto-mas-vendido-zona', estadisticasController.getProductoMasVendidoPorZona);
 
 module.exports = router;
